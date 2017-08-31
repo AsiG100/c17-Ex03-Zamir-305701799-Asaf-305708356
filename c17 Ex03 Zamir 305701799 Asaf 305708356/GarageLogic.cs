0a12ce8cfbd25869eca3b6fd297df66a83caa3a0
AsiG <Asi.g100@gmail.com>
2017-08-31 16:23:44 +0300

    all the new file i made

---
+++ c17 Ex03 Zamir 305701799 Asaf 305708356/GarageLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c17_Ex03_Zamir_305701799_Asaf_305708356
+{
+    public class GarageLogic
+    {
+        //TODO all the logic methods we planned after building the BE classes
+    }
+}
---
+++ c17 Ex03 Zamir 305701799 Asaf 305708356/ValueOutOfRangeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c17_Ex03_Zamir_305701799_Asaf_305708356
+{
+    class ValueOutOfRangeException : Exception
+    {
+        float m_MaxValue;
+        float m_MinValue;
+
+        public ValueOutOfRangeException(string i_Messege, float i_Max, float i_Min)
+                                         :base(i_Messege)
+        {
+            this.m_MaxValue = i_Max;
+            this.m_MinValue = i_Min;
+        }
+    }
+}
