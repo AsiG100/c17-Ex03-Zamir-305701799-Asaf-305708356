dd7a860385ce5bbd853c6ec2248e40dbcb5b8fe0
zamirMarkovsky <16.zamir@gmail.com>
2017-09-01 19:36:41 +0300

    removed un-used using methods

--- c17 Ex03 Zamir 305701799 Asaf 305708356/Fuel.cs
+++ c17 Ex03 Zamir 305701799 Asaf 305708356/Fuel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Ex03.GarageLogic.Enums;
+﻿using Ex03.GarageLogic.Enums;
 
 namespace Ex03.GarageLogic
 {
--- c17 Ex03 Zamir 305701799 Asaf 305708356/ValueOutOfRangeException.cs
+++ c17 Ex03 Zamir 305701799 Asaf 305708356/ValueOutOfRangeException.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace c17_Ex03_Zamir_305701799_Asaf_305708356
 {
